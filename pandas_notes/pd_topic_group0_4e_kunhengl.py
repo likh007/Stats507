21b8c5ff010e11927da7ad01d56c31f12440774d
kunhengl <89876938+likh007@users.noreply.github.com>
2021-11-12 15:54:08 -0500

    Create pd_topic_group0_4e_kunhengl.py

---
+++ pandas_notes/pd_topic_group0_4e_kunhengl.py
@@ -0,0 +1,36 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_json: true
+#     notebook_metadata_filter: markdown
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# ## Topics in Pandas
+# **Stats 507, Fall 2021** 
+#   
+
+# ## Contents
+# Add a bullet for each topic and link to the level 2 title header using 
+# the exact title with spaces replaced by a dash. 
+#
+# + [Topic Title](#Topic-Title) 
+# + [Topic 2 Title](#Topic-2-Title)
+
+# + [Data Expansion](./pd_topic_kunhengl.py)
+
+# ## Topic Title
+# Include a title slide with a short title for your content.
+# Write your name in *bold* on your title slide.
+
+# ## Data Expansion
+# ### Using pandas to transfrom one row to many rows.  *Kunheng Li*
+
